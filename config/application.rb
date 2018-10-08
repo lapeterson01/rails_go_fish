a1a11ef11b571e3290c32898a134cefa55980c36
Logan Peterson <lapeterson01@gmail.com>
2018-10-08 15:19:16 -0400

    update config/application.rb

--- config/application.rb
+++ config/application.rb
@@ -21,6 +21,7 @@ Bundler.require(*Rails.groups)
 module RailsGoFish
   class Application < Rails::Application
     config.assets.initialize_on_precompile = false
+    config.assets.enabled = true
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.1
 
