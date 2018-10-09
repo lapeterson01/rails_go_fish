b0b1dc3f1e4cc40fb674650ef6e57f95f9c28e09
Logan Peterson <lapeterson01@gmail.com>
2018-10-09 11:47:51 -0400

    removed password field for now

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -2,8 +2,7 @@ require 'rails_helper'
 
 RSpec.describe User, type: :model do
   before do
-    @user = User.new name: 'Test User', username: 'testuser97', password: 'foobar',
-                     password_confirmation: 'foobar'
+    @user = User.new(name: 'Test User', username: 'testuser97')
   end
 
   it 'should be valid' do
@@ -39,18 +38,6 @@ RSpec.describe User, type: :model do
       @user.save
       expect(duplicate_user).to_not be_valid
       @user.destroy
-    end
-  end
-
-  describe 'password' do
-    it 'should not be blank' do
-      @user.password = @user.password_confirmation = '      '
-      expect(@user).to_not be_valid
-    end
-
-    it 'should not be too short' do
-      @user.password = @user.password_confirmation = 'a' * 5
-      expect(@user).to_not be_valid
     end
   end
 end
