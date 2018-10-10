241d63406ae56feff8049575cde9f5678df88abf
Logan Peterson <lapeterson01@gmail.com>
2018-10-10 14:57:03 -0400

    added card_deck model

---
+++ app/models/card_deck.rb
@@ -0,0 +1,30 @@
+class CardDeck
+  attr_reader :cards
+
+  RANKS = %w[A K Q J 10 9 8 7 6 5 4 3 2].freeze
+  SUITS = %w[Spades Clubs Diamonds Hearts].freeze
+
+  def initialize(cards = RANKS.map { |rank| SUITS.map { |suit| PlayingCard.new(rank, suit) } })
+    @cards = cards.flatten
+  end
+
+  def shuffle!
+    @cards.shuffle!
+  end
+
+  def deal
+    @cards.shift
+  end
+
+  def out_of_cards?
+    @cards.empty?
+  end
+
+  def ==(other)
+    equal = true
+    other.cards.each do |card2|
+      equal = false if cards[other.cards.index(card2)] != card2
+    end
+    equal
+  end
+end
---
+++ spec/models/card_deck_spec.rb
@@ -0,0 +1,46 @@
+require 'rails_helper'
+
+RSpec.describe CardDeck, type: :model do
+  describe '#initialize' do
+    it 'creates a deck of 52 standard playing cards' do
+      deck = CardDeck.new
+      expect(deck.cards.length).to eq 52
+    end
+  end
+
+  describe '#shuffle!' do
+    it 'shuffles the deck of cards' do
+      deck1 = CardDeck.new
+      deck2 = CardDeck.new
+      expect(deck1).to eq deck2
+      deck1.shuffle!
+      expect(deck1).to_not eq deck2
+    end
+  end
+
+  describe 'equality' do
+    it 'allows two instances of CardDeck to be equal if the cards are equal' do
+      deck1 = CardDeck.new
+      deck2 = CardDeck.new
+      expect(deck1).to eq deck2
+    end
+  end
+
+  describe '#deal' do
+    it 'returns the top card from the deck' do
+      deck = CardDeck.new
+      card = deck.deal
+      expect(card).to eq PlayingCard.new('A', 'Spades')
+      expect(deck.cards.length).to eq 51
+    end
+  end
+
+  describe '#out_of_cards?' do
+    it 'returns true if the deck is out of cards and false if it is not' do
+      deck1 = CardDeck.new
+      expect(deck1.out_of_cards?).to eq false
+      deck2 = CardDeck.new([])
+      expect(deck2.out_of_cards?).to eq true
+    end
+  end
+end
